b4bc937293812b9bf5f028ac946c23db35eb76c3
Izuku-mi <151708571+izuku-mii@users.noreply.github.com>
2025-12-04 16:40:38 +0700

    🟠 UPDATE FITUR: Instagram downloader!

--- plugins/downloader/instagram.js
+++ plugins/downloader/instagram.js
@@ -1,17 +1,15 @@
 let old = new Date()
-
-import * as baileys from "@adiwajshing/baileys";
 import axios from "axios";
 
-const izuku = async (m, {
+export default async function izuku(m, {
     conn,
     text,
     usedPrefix,
     command,
     args
-}) => {
+}) {
     const linkig = text.includes('ins')
-    if (!linkig) throw `Maaf Anda Masukkan Link Dulu Contoh ${usedPrefix + command} https://www.instagram.com/xxxx`;
+    if (!linkig) m.reply(`Maaf Anda Masukkan Link Dulu Contoh ${usedPrefix + command} https://www.instagram.com/xxxx`);
 
     let url;
     if (linkig) {
@@ -20,74 +18,30 @@ const izuku = async (m, {
                 result: ig
             } = await (await fetch(apikey.izumi + '/downloader/instagram?url=' + encodeURIComponent(args[0]))).json();
             if (ig.isVideo === false) {
-                const ft = ig.media.map(v => v);
-                let push = [];
-                for (let i = 0; i < ft.length; i++) {
-                    let cap = `☘️ *Process*: ${((new Date - old) * 1)} ms`;
-                    const mediaMessage = await baileys.prepareWAMessageMedia({
+                const slide = ig.media
+                if (slide.length > 1) {
+                    const list = slide.map(v => ({
                         image: {
-                            url: ft[i]
+                            url: v,
+                            caption: `☘️ *Process*: ${((new Date - old) * 1)} ms`
                         }
-                    }, {
-                        upload: conn.waUploadToServer
+                    }));
+
+                    const alse = await conn.sendAlbum(m.chat, list, {
+                        delay: 500,
+                        quoted: m
                     });
-                    push.push({
-                        body: baileys.proto.Message.InteractiveMessage.Body.fromObject({
-                            text: cap
-                        }),
-                        footer: baileys.proto.Message.InteractiveMessage.Footer.fromObject({
-                            text: botname
-                        }),
-                        header: baileys.proto.Message.InteractiveMessage.Header.create({
-                            title: `☘️ *Slide*: ${i + 1}/${ft.length}`,
-                            subtitle: '',
-                            hasMediaAttachment: true,
-                            ...mediaMessage
-                        }),
-                        nativeFlowMessage: baileys.proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
-                            buttons: [{}]
-                        })
+                    await conn.reply(m.chat, `☘️ *Process*: ${((new Date - old) * 1)} ms`, alse);
+                } else {
+                    await conn.sendMessage(m.chat, {
+                        image: {
+                            url: slide[0]
+                        },
+                        caption: `☘️ *Process*: ${((new Date - old) * 1)} ms`
+                    }, {
+                        quotes: m
                     });
                 }
-                const msg = baileys.generateWAMessageFromContent(m.chat, {
-                    viewOnceMessage: {
-                        message: {
-                            messageContextInfo: {
-                                deviceListMetadata: {},
-                                deviceListMetadataVersion: 2
-                            },
-                            interactiveMessage: baileys.proto.Message.InteractiveMessage.fromObject({
-                                body: baileys.proto.Message.InteractiveMessage.Body.create({
-                                    text: ownername
-                                }),
-                                footer: baileys.proto.Message.InteractiveMessage.Footer.create({
-                                    text: botname
-                                }),
-                                header: baileys.proto.Message.InteractiveMessage.Header.create({
-                                    hasMediaAttachment: false
-                                }),
-                                carouselMessage: baileys.proto.Message.InteractiveMessage.CarouselMessage.fromObject({
-                                    cards: push
-                                }),
-                                contextInfo: {
-                                    mentionedJid: [m.sender],
-                                    forwardingScore: 999,
-                                    isForwarded: true,
-                                    forwardedNewsletterMessageInfo: {
-                                        newsletterJid: saluran,
-                                        newsletterName: ownername,
-                                        serverMessageId: 143
-                                    }
-                                }
-                            })
-                        }
-                    }
-                }, {
-                    quoted: m
-                });
-                await conn.relayMessage(m.chat, msg.message, {
-                    messageId: msg.key.id
-                });
             } else {
                 let {
                     data: res
@@ -110,5 +64,3 @@ izuku.tags = ['downloader'];
 izuku.command = ['instagram', 'igdl', 'ig', 'igdl'];
 izuku.loading = true;
 izuku.limit = true;
-
-export default izuku;
